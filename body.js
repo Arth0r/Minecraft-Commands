ddcc07fb17e6e0a6141776afdddf289369ed0fcc
Arthur <46944714+Arth0r@users.noreply.github.com>
2019-01-22 21:54:56 -0200

    Create body.js

---
+++ body.js
@@ -0,0 +1,26 @@
+module.exports = new (class cmd{
+    constructor(){
+        this.name = "body"; // nome do comando
+        this.category = "util" // estilo da categoria
+        this.help = "Veja a minha lista de comandos"; // sistema de help do cmd
+        this.cooldown = 3; //delay
+        this.cdMessage = "Aguarde 3 segundos para usar o comando novamente"; // mensagem
+        this.aliases = []  // aliases
+    }
+    run({message,buildMessage,client,args}){
+var player = args.join(' ')
+if(!player) return message.reply("voce deve me dar um nick");
+ var url = `https://minotar.net/body/` + `${player}` + `/100.png`
+ message.channel.send({
+     embed: {
+         image:{
+             url: url
+         },
+         author: {
+             name: `${message.author.username}#${message.author.discriminator} esta aqui o resultado da sua pesquisa`
+         },
+         color: 0x00FFFF
+        }
+    })
+    }
+})
