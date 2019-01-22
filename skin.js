720e29847e3f22291223d048e7cb122f1b283ab9
Arthur <46944714+Arth0r@users.noreply.github.com>
2019-01-22 21:57:44 -0200

    Create skin.js

---
+++ skin.js
@@ -0,0 +1,26 @@
+module.exports = new (class cmd{
+    constructor(){
+        this.name = "skin"; // nome do comando
+        this.category = "minecraft" // estilo da categoria
+        this.help = "Veja a minha lista de comandos"; // sistema de help do cmd
+        this.cooldown = 3; //delay
+        this.cdMessage = "Aguarde 3 segundos para usar o comando novamente"; // mensagem
+        this.aliases = ["ajuda","cmds","commands"]  // aliases
+    }
+    run({message,buildMessage,client,args}){
+         var player = args.join(' ')
+        if(!player) return message.reply("voce deve me dar um nick");
+         var url = `https://minotar.net/skin/` + `${player}`
+         message.channel.send({
+             embed: {
+                 image:{
+                     url: url
+                 },
+                 author: {
+                     name: `${message.author.username}#${message.author.discriminator} esta aqui o resultado da sua pesquisa`
+                 },
+                 color: 0x00FFFF
+                }
+            })
+    }
+})
